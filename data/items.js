75f4f8fa51bf980a6eba6fa26d460b810e5d5d90
terzzzz <tercheng@gmail.com>
2026-08-18 08:58:04 +0800

    Update items.js

--- data/items.js
+++ data/items.js
@@ -1,1 +1,31 @@
 
+[
+  {
+    "id": "tachibana_drink",
+    "name": "Tachibana Soda",
+    "type": "HEAL_LP",
+    "value": 200,
+    "description": "Restores +200 LP instantly upon winning turn."
+  },
+  {
+    "id": "wind_capsule",
+    "name": "Typhoon Capsule",
+    "type": "GAIN_CHI",
+    "value": 8,
+    "description": "Fills +8 CHI directly into meter."
+  },
+  {
+    "id": "power_orb",
+    "name": "SFC Power Orb",
+    "type": "BUFF_ATK",
+    "value": 1.30,
+    "description": "+30% Damage bonus on next landing attack."
+  },
+  {
+    "id": "cyber_battery",
+    "name": "Cybernetic Drainer",
+    "type": "DRAIN_CHI",
+    "value": 5,
+    "description": "Drains -5 CHI from opponent meter."
+  }
+]
